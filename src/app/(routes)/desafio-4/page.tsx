170ca2186db42eb7bc6443ad0c1a83361be9a94a
Dario Reis <guttyerresjunior1995@gmail.com>
2023-08-28 15:55:52 -0300

    feat:challenge 4 completed

---
+++ src/app/(routes)/desafio-4/page.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { RefreshCcw } from "lucide-react"
+
+export default function Desafio4() {
+  const [primeiroNumero, setPrimeiroNumero] = useState(0);
+  const [segundoNumero, setSegundoNumero] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  const somar: (e: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    e.preventDefault();
+    setResultado(primeiroNumero + segundoNumero)
+  }
+
+
+  const refreshPage = () => {
+    window.location.reload()
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h1 className="text-2xl mt-7 mb-7">Desafio 04</h1>
+      <p className="text-base text-justify m-4">
+        Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório entre eles.<br />
+        Ex: <br />
+        Digite um valor: 8 <br />
+        Digite outro valor: 5 <br />
+        A Soma entre 8 e 5 é igual a 13.
+      </p>
+
+      <div
+        className="w-[320px] min-h-[300px] md:w-[600px] xl:w-[1000px] pt-3 pb-3 border-2 flex items-center justify-center"
+      >
+        <form>
+          <Label className=" text-base">
+            Digite um valor
+          </Label>
+          <Input
+            className="w-[300] max-w-[600px] mb-4 mt-4"
+            type="number"
+            min={0}
+            value={primeiroNumero}
+            onChange={(e) => setPrimeiroNumero(Number(e.target.value))}
+          />
+          <Label className=" text-base">
+            Digite outro valor
+          </Label>
+          <Input
+            className="w-[300] max-w-[600px] mb-4 mt-4"
+            type="number"
+            min={0}
+            value={segundoNumero}
+            onChange={(e) => setSegundoNumero(Number(e.target.value))}
+          />
+          <Button type="submit" variant={"secondary"} size={"lg"} onClick={somar}>
+            Enviar
+          </Button>
+          {resultado === 0 ? (
+            <></>
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="md:text-2xl text-justify p-4">A Soma entre <span className="font-bold">{primeiroNumero}</span> e <span className="font-bold">{segundoNumero}</span>  é igual a <span className="font-bold">{resultado}</span></p>
+              <Button variant={"outline"} size={"icon"} onClick={refreshPage}>
+                <RefreshCcw />
+              </Button>
+            </div>
+          )}
+
+
+
+
+
+        </form>
+
+
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,8 +41,107 @@ export default function Home() {
             </Button>
           </CardFooter>
         </Card>
+        <Card className="w-[400px] md:w-[300px]">
+          <CardHeader>
+            <CardTitle>Desafio 02</CardTitle>
+            <CardDescription>Resumo do desafio de lógica</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-between">
+            <Button>
+              <Link
+                href="https://github.com/darioreisjr/desafiodelogicadeprogramacao/tree/main/src/app/(routes)/desafio-2"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+            >
+              <Link
+                href="/desafio-2"
+              >
+                Deploy
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="w-[400px] md:w-[300px]">
+          <CardHeader>
+            <CardTitle>Desafio 03</CardTitle>
+            <CardDescription>Resumo do desafio de lógica</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-between">
+            <Button>
+              <Link
+                href="https://github.com/darioreisjr/desafiodelogicadeprogramacao/tree/main/src/app/(routes)/desafio-3"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+            >
+              <Link
+                href="/desafio-3"
+              >
+                Deploy
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="w-[400px] md:w-[300px]">
+          <CardHeader>
+            <CardTitle>Desafio 04</CardTitle>
+            <CardDescription>Resumo do desafio de lógica</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-between">
+            <Button>
+              <Link
+                href="https://github.com/darioreisjr/desafiodelogicadeprogramacao/tree/main/src/app/(routes)/desafio-4"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+            >
+              <Link
+                href="/desafio-4"
+              >
+                Deploy
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="w-[400px] md:w-[300px]">
+          <CardHeader>
+            <CardTitle>Desafio 05</CardTitle>
+            <CardDescription>Resumo do desafio de lógica</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-between">
+            <Button>
+              <Link
+                href="https://github.com/darioreisjr/desafiodelogicadeprogramacao/tree/main/src/app/(routes)/desafio-5"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+            >
+              <Link
+                href="/desafio-5"
+              >
+                Deploy
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
-
     </>
   )
 }
