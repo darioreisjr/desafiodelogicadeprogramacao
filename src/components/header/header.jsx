3534da365a27a3543386079eb786cfbcee60a678
Dario Reis <guttyerresjunior1995@gmail.com>
2023-09-10 22:41:49 -0300

    feat: add icon menubar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,43 +4,46 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar"
 import Link from "next/link";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+
+import { Github, Home, Swords, User2, GalleryHorizontalEnd,Linkedin } from 'lucide-react';
+
 
 const Header = () => {
   return (
     <Menubar className="max-w-7xl  flex items-center justify-center p-16">
       <MenubarMenu>
-        <MenubarTrigger>
+        <MenubarTrigger className="space-x-1">
+        <Home size={20} />
           <Link href="/">
             Inicio
           </Link>
         </MenubarTrigger>
-        <MenubarTrigger>
+        <MenubarTrigger className="space-x-1">
+          <Swords size={20}/>
           <Link href="/">
             Desafios
           </Link>
         </MenubarTrigger>
-        <MenubarTrigger>
+        <MenubarTrigger className="space-x-1">
+          <User2 size={20}/>
           <Link href="/">
             Contatos
           </Link>
         </MenubarTrigger>
-        <MenubarTrigger>
-          <Link href="https://github.com/darioreisjr" target="_blank">
-            GitHub
+        <MenubarTrigger className="space-x-1">
+          <GalleryHorizontalEnd size={20}/>
+          <Link href="https://darioreisjr.vercel.app/" target="_blank">
+            Portifolio
           </Link>
         </MenubarTrigger>
         <MenubarTrigger>
-          <Link href="https://darioreisjr.vercel.app/" target="_blank">
-            Portifolio
+          <Link href="https://github.com/darioreisjr" target="_blank">
+            <Github />
+          </Link>
+        </MenubarTrigger>
+        <MenubarTrigger>
+          <Link href="https://www.linkedin.com/in/darioreisjr" target="_blank">
+            <Linkedin  />
           </Link>
         </MenubarTrigger>
       </MenubarMenu>
