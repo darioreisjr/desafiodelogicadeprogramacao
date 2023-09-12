5a2cffe619d565385e6e6f200621f1e075b1ccc9
Dario Reis <guttyerresjunior1995@gmail.com>
2023-09-12 11:26:32 -0300

    feat: challenge eighteen complete check age to vote

--- src/app/(routes)/desafio-18/page.jsx
+++ src/app/(routes)/desafio-18/page.jsx
@@ -7,26 +7,26 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { RefreshCcw } from "lucide-react"
 
-export default function Desafio17() {
-  const [velocidadeCarro, setVelocidadeCarro] = useState(0);
-  const [velocidadeChecada, setVelocidadeChecada] = useState(0);
+export default function Desafio18() {
+  const [anoNascimento, setAnoNadcimento] = useState(0);
+  const [idadevotar, setIdadeVotar] = useState(0);
 
 
 
-  const calculateSpeed = (event) => {
+  const checkIfYouCanVote = (event) => {
     event.preventDefault();
+
+    const idade =  2023 - anoNascimento
   
-    if (velocidadeCarro > 80) {
-      const multa = velocidadeCarro - 80
-      const valorMulta = multa * 5
-      setVelocidadeChecada(
-        `Você foi multado em R$ ${valorMulta} por excesso de velocidade. A velocidade máxima permitida é de 80 km/h. Você estava a ${velocidadeCarro}km/h.`
+    if (idade >= 18) {
+      setIdadeVotar(
+        "Você agora é um eleitor e pode contribuir para o futuro do nosso país. Parabéns!"
       );
       
     }
     else {
-      setVelocidadeChecada(
-        `Você está dentro da velocidade permitida.`
+      setIdadeVotar(
+        "Nesta eleição, você não pode votar, pois ainda não tem idade suficiente. Mas você pode acompanhar as eleições e aprender mais sobre o processo eleitoral."
       );
     }
 
@@ -38,37 +38,34 @@ export default function Desafio17() {
 
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="text-2xl mt-7 mb-7">Desafio 17</h1>
+      <h1 className="text-2xl mt-7 mb-7">Desafio 18</h1>
       <p className="text-base text-justify m-4">
-        Escreva um programa que pergunte a velocidade de um carro. Caso ultrapasse <br />
-        80Km/h, exiba uma mensagem dizendo que o usuario foi multado. Nesse caso,  <br />
-        exiba o valor da multa, cobrando R$5,00 por cada Km acima da velocidade <br />
-        permitida.
+        Faça um programa que leia o ano de nascimento de uma pessoa, calcule a idade dela <br /> e despois mostre se ela pode ou não votar.
       </p>
 
       <div
         className="w-[320px] min-h-[300px] md:w-[600px] xl:w-[1000px] pt-3 pb-3 border-2 flex items-center justify-center"
       >
         <form>
-          {!velocidadeChecada ? (
+          {!idadevotar ? (
             <>
               <Label className=" text-base ">
-                Qual a velocidade do carro?
+                Qual seu ano de nascimento?
               </Label>
               <Input
                 className="w-[300] max-w-[600px] mb-4 mt-4"
                 type="number"
                 min={0}
-                onChange={(event) => setVelocidadeCarro(Number(event.target.value))}
+                onChange={(event) => setAnoNadcimento(Number(event.target.value))}
               />
-              <Button type="submit" variant={"default"} size={"lg"} onClick={calculateSpeed}>
+              <Button type="submit" variant={"default"} size={"lg"} onClick={checkIfYouCanVote}>
                 Checar 
               </Button>
             </>
           ) : (
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="md:text-2xl text-center p-4 font-bold">
-                {velocidadeChecada}
+                {idadevotar}
               </p>
               <Button variant={"outline"} size={"icon"} onClick={refreshPage}>
                 <RefreshCcw />
